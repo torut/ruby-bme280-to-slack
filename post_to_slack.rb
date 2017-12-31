0f48a550931bd77e849c8fe8bd58114ac9d481b6
Toru TAMURA <torut@ttsoft.jp>
2017-12-31 15:43:15 +0900

    [add] threshold temperature.
    高温、低温の閾値を設定してそれらを越えたor下回りましたときだけslackにpostする.

--- post_to_slack.rb
+++ post_to_slack.rb
@@ -11,12 +11,21 @@ require "time"
 WEBHOCKURL = "<slack-incoming-webhock-url>"
 CHANNEL = "#general"
 
+# slackにpostする閾値
+THRESHOLD_HOT = 30;             # この温度を越えたら
+THRESHOLD_COLD = 10;            # この温度を下回ったら
+
 # BME280に接続
 i2cbus = RPi::I2CBus.new(1)
 bme = RPi::BME280.new(i2cbus)
 
 # センサーからの値を更新
 bme.update
+
+if bme.temperature <= THRESHOLD_HOT && bme.temperature >= THRESHOLD_COLD
+  # 高温閾値より低い and 低温閾値より高い
+  exit
+end
 
 require 'slack-notifier'
 
@@ -53,6 +62,13 @@ attachment[:color] = '#33ccff' if bme.temperature < 20;
 attachment[:color] = '#3333ff' if bme.temperature < 10;
 attachment[:color] = '#3300ff' if bme.temperature < 0;
 
+if bme.temperature <= THRESHOLD_COLD
+  attachment[:text] += "\n低温閾値(#{THRESHOLD_COLD}℃)を下回りました。"
+end
+
+if bme.temperature >= THRESHOLD_HOT
+  attachment[:text] += "\n高温閾値(#{THRESHOLD_COLD}℃)を上回りました。"
+end
 
 notifier = Slack::Notifier.new WEBHOCKURL,
                                channel: CHANNEL,
